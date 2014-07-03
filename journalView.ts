bc4829dc8ef4e2e1b96e894d5174ec5ed9518dad
edsilv <e.silverton@gmail.com>
2014-07-03 18:11:36 +0100

    added journalView

---
+++ journalView.ts
@@ -0,0 +1,162 @@
+/// <reference path="../../js/jquery.d.ts" />
+/// <reference path="../../js/extensions.d.ts" />
+
+import utils = require("../../utils");
+import baseExtension = require("../coreplayer-shared-module/baseExtension");
+import shell = require("../coreplayer-shared-module/shell");
+import baseView = require("../coreplayer-shared-module/baseView");
+import TreeNode = require("../coreplayer-shared-module/treeNode");
+
+export class JournalView extends baseView.BaseView {
+
+    $tree: JQuery;
+    selectedStructure: any;
+
+    rootNode: TreeNode;
+
+    static VIEW_STRUCTURE: string = 'treeView.onViewStructure';
+    static VIEW_MANIFEST: string = 'treeView.onViewManifest';
+
+    constructor($element: JQuery) {
+        super($element, true, true);
+    }
+
+    create(): void {
+        super.create();
+
+        $.subscribe(baseExtension.BaseExtension.CANVAS_INDEX_CHANGED, (e, canvasIndex) => {
+            this.selectIndex(canvasIndex);
+        });
+
+        this.rootNode = this.provider.getJournalTree();
+
+        if (!this.rootNode) return;
+
+        this.$tree = $('<ul class="tree"></ul>');
+        this.$element.append(this.$tree);
+
+        $.templates({
+            pageTemplate: '{^{for nodes}}\
+                               {^{tree/}}\
+                           {{/for}}',
+            treeTemplate: '<li>\
+                               {^{if nodes && nodes.length}}\
+                                   {^{if expanded}}\
+                                       <div class="toggle expanded"></div>\
+                                   {{else}}\
+                                       <div class="toggle"></div>\
+                                   {{/if}}\
+                               {{else}}\
+                                   <div class="spacer"></div>\
+                               {{/if}}\
+                               {^{if selected}}\
+                                   <a href="#" class="selected">{{>label}}</a>\
+                               {{else}}\
+                                   <a href="#">{{>label}}</a>\
+                               {{/if}}\
+                           </li>\
+                           {^{if expanded}}\
+                               <li>\
+                                   <ul>\
+                                       {^{for nodes}}\
+                                           {^{tree/}}\
+                                       {{/for}}\
+                                   </ul>\
+                               </li>\
+                           {{/if}}'
+        });
+
+        $.views.tags({
+            tree: {
+                toggle: function () {
+                    $.observable(this.data).setProperty("expanded", !this.data.expanded);
+                },
+                init: function (tagCtx, linkCtx, ctx) {
+                    this.data = tagCtx.view.data;
+                },
+                onAfterLink: function () {
+                    var self = this;
+
+                    self.contents("li").first()
+                        .on("click", ".toggle", function () {
+                            self.toggle();
+                        }).on("click", "a", function (e) {
+                            e.preventDefault();
+
+                            if (self.data.type == 'manifest') {
+                                $.publish(JournalView.VIEW_MANIFEST, [self.data.ref]);
+                            } else {
+                                $.publish(JournalView.VIEW_STRUCTURE, [self.data.ref]);
+                            }
+                        })
+                },
+                template: $.templates.treeTemplate
+            }
+        });
+
+        this.$tree.link($.templates.pageTemplate, this.rootNode);
+
+        this.resize();
+    }
+
+    selectIndex(index: number): void {
+        // may be authenticating
+        if (index == -1) return;
+
+        // has a tree been successfully generated?
+        if (!this.rootNode) return;
+
+        var structure = this.provider.getStructureByCanvasIndex(index);
+
+        if (!structure) return;
+
+        this.selectPath(structure.path);
+    }
+
+    selectPath(path: string): void {
+
+        var pathArr = path.split("/");
+
+        if (pathArr.length >= 1) pathArr.shift();
+
+        // reset the previous selected node.
+        if (this.selectedStructure) $.observable(this.selectedStructure.treeNode).setProperty("selected", false);
+
+        // should have an array that looks like this: [0, 1, 0]
+        // (1st structure in root, 2nd structure in that, 1st structure in that).
+        // recursively walk tree to set selected node.
+        this.selectedStructure = this.getStructure(this.provider.getRootStructure(), pathArr);
+
+        $.observable(this.selectedStructure.treeNode).setProperty("selected", true);
+    }
+
+    show(): void {
+        this.$element.show();
+
+        setTimeout(() => {
+            this.selectIndex(this.provider.canvasIndex);
+        }, 1);
+    }
+
+    hide(): void {
+        this.$element.hide();
+    }
+
+    getStructure(parentStructure, path) {
+
+        if (path.length == 0) return parentStructure;
+
+        parentStructure.expanded = true;
+
+        var index = path.shift();
+
+        var structure = this.provider.getStructureByIndex(parentStructure, index);
+
+        return this.getStructure(structure, path);
+    }
+
+    resize(): void {
+        super.resize();
+
+    }
+}
--- treeViewLeftPanel.ts
+++ treeViewLeftPanel.ts
@@ -2,6 +2,8 @@
 
 import basePanel = require("../coreplayer-treeviewleftpanel-module/treeViewLeftPanel");
 import utils = require("../../utils");
+import journal = require("./journalView");
+import tree = require("../coreplayer-treeviewleftpanel-module/treeView");
 
 export class TreeViewLeftPanel extends basePanel.TreeViewLeftPanel {
 
@@ -14,5 +16,16 @@ export class TreeViewLeftPanel extends basePanel.TreeViewLeftPanel {
         this.setConfig('treeViewLeftPanel');
 
         super.create();
+    }
+
+    createTreeView(): void {
+
+        var manifestType = this.provider.getManifestType();
+
+        if (manifestType.toLowerCase() === "periodicalissue"){
+            this.treeView = new journal.JournalView(this.$treeView);
+        } else {
+            this.treeView = new tree.TreeView(this.$treeView);
+        }
     }
 }
